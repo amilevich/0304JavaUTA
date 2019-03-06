773a21f48e5e1cfdb657faad3faa7b5e72228eda
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-06 04:21:37 -0600

    Update BubbleSort.java

--- Andrew_Peklar_Code/JavaHomeWork/src/Q1/BubbleSort.java
+++ Andrew_Peklar_Code/JavaHomeWork/src/Q1/BubbleSort.java
@@ -5,18 +5,14 @@ import java.util.Arrays;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-
 		int[] array = { 1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4 };
 		BubbleSort(array, array.length);
 		System.out.println(Arrays.toString(array));
-
 	}
 
 	public static void BubbleSort(int[] array, int n) {
 		// base case
-		if (n == 1)
-			return;
-
+		if (n == 1)	return;
 		// one pass of bubble sort
 		// largest element moved to end
 		for (int i = 0; i < n - 1; i++)
@@ -27,8 +23,7 @@ public class BubbleSort {
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 			}
-		// recursion call
-		// decrement list length until == 1
+		// recursively decrement list length until == 1
 		BubbleSort(array, n - 1);
 	}
 }
