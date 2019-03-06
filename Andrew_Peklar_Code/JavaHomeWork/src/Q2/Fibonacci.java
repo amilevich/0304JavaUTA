1bc7a534c51e0774412d4d547487f0a08afe0fa4
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-06 04:24:14 -0600

    Update Fibonacci.java

--- Andrew_Peklar_Code/JavaHomeWork/src/Q2/Fibonacci.java
+++ Andrew_Peklar_Code/JavaHomeWork/src/Q2/Fibonacci.java
@@ -7,21 +7,18 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		firstTwentFiveFibs();
 	}
-	
 	//this method prints the first 25 fibonacchi numbers
-	//can be modified to print any number by adding a parameter
-	//for the while statement.
 	private static void firstTwentFiveFibs() {
-		int tmp = -1;			//index var
+		int tmp = -1;		//index var
 		int array[] = {1, 2}; 	//array to hold answers
 		System.out.println(1);	//print first output
 		
 		while(++tmp != 25) { 
 			System.out.println(array[tmp&1]);
-			//short-curcuit and checks last bit of binary
-			//switches where new sum stored each loop
+			//short-curcuit to check last bit of binary
+			//two (2) indices that switch storing the new sum
 			if ((tmp & 1) == 0)	array[0] = array[1] + array[0];
-			else				array[1] = array[1] + array[0];	
+			else			array[1] = array[1] + array[0];	
 		}
 	}
 }
