70d24d7249ac2cc8a5d45ec722500f8f0f11d706
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-09 11:39:51 -0600

    Update Palindromes.java

--- Andrew_Peklar_Code/JavaHomeWork/src/question08/Palindromes.java
+++ Andrew_Peklar_Code/JavaHomeWork/src/question08/Palindromes.java
@@ -13,23 +13,23 @@ public class Palindromes {
 	
 	//Strings to sort
 	private static String[] wordBank  = {	"karan",	"madam",	"tom",		"civic",
-											"radar",	"sexes",	"jimmy",	"kayak", 
-											"john",		"refer",	"billy",	"did"	};
+						"radar",	"sexes",	"jimmy",	"kayak", 
+						"john",		"refer",	"billy",	"did"	 };
 	
 	public static void main(String[] args) {
 		//for each item in wordBank, add to either list 
 		//depending on if it is a palindrome or not
 		Arrays.stream(wordBank)
-			  .forEach((x) -> ((isPalindrome(x)) ? Palindromes : nonPalindromes).add(x));
+		      .forEach((x) -> ((isPalindrome(x)) ? Palindromes : nonPalindromes).add(x));
 		
 		//print both lists to check
-		  System.out.println(Palindromes);
-		  System.out.println(nonPalindromes);
+		System.out.println(Palindromes);
+		System.out.println(nonPalindromes);
 	}
 	
 	private static boolean isPalindrome(String s) {
 		//check half the word
 		return IntStream.range(0, s.length()/2)
-						.allMatch(i -> s.charAt(i) == s.charAt(s.length()-i-1));
+				.allMatch(i -> s.charAt(i) == s.charAt(s.length()-i-1));
 	}
 }
