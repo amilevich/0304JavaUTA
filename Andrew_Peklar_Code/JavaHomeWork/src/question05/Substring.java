63c141f4a80edd4530a976faf34a179322aeb27a
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-09 11:49:50 -0600

    Update Substring.java

--- Andrew_Peklar_Code/JavaHomeWork/src/question05/Substring.java
+++ Andrew_Peklar_Code/JavaHomeWork/src/question05/Substring.java
@@ -7,18 +7,16 @@ public class Substring {
 	public static void main(String[] args) {
 		String tmp = "hello";
 		String subTmp = subString(tmp, 3);
-		System.out.println(subTmp);
-		
+		System.out.println(subTmp);	
 	}
 	
-	//can do with IntStream via ".char" as well.
-	//need this extra step though".mapToObj(c -> (char)c)"
-	//".collect(Collectors.joining(""))" works for final step, 
-	//however, it uses stringBuilder;
 	public static String subString(String strg, int n) {
 		return Arrays.stream(strg.split(""))
         		     .skip(n)
         		     .map(Object::toString)
         		     .reduce("", String::concat); 
 	}
-}
+}	//can do with IntStream via ".char" as well.
+	//need this extra step though".mapToObj(c -> (char)c)"
+	//".collect(Collectors.joining(""))" works for final step, 
+	//however, it uses stringBuilder;
