855a442a0ec4c3b96058da703a5fbbc76f1da694
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-09 11:42:24 -0600

    Update Factorial.java

--- Andrew_Peklar_Code/JavaHomeWork/src/question04/Factorial.java
+++ Andrew_Peklar_Code/JavaHomeWork/src/question04/Factorial.java
@@ -14,7 +14,7 @@ public class Factorial {
 		if (n == 0)	return 1;	//special case: 0! = 1
 		int ans = 1; 			//var to track answer
 		while(n != 0)			//decrementing after captures 1! case
-			ans *= n--;			//ans = ans * n and decrements n
+			ans *= n--;		//ans = ans * n and decrements n
 		return ans;
 	}
 	
