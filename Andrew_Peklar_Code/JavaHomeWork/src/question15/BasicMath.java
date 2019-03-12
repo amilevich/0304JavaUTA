8e940a9ae4ccf4d2da39c6faee7bf01f52631361
Andrew Peklar <andrewpeklar@gmail.com>
2019-03-11 20:45:29 -0600

    removed duplicate directory

--- Andrew_Peklar_Code/JavaHomeWork/src/question15/BasicMath.java
+++
@@ -1,8 +0,0 @@
-package question15;
-
-public interface BasicMath {
-	double add(double a, double b);
-	double sub(double a, double b);
-	double div(double a, double b);
-	double mlt(double a, double b);
-}
--- Andrew_Peklar_Code/JavaHomeWork/src/question18/HighClass.java
+++
@@ -1,8 +0,0 @@
-package question18;
-
-public abstract class HighClass {
-	
-	public abstract boolean isItUppercase(String str);
-	public abstract String makeUppercase(String str);
-	public abstract int stringToInt(String str);
-}
