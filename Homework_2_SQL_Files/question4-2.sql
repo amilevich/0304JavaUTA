98e2680246110a265acd4815a47e7ca9acb9bc84
Angello Castro <angello.castro.002@gmail.com>
2019-03-15 09:18:58 -0700

    Homework 2 updated

--- Homework_2_SQL_Files/question4-2.sql
+++ Homework_2_SQL_Files/question4-2.sql
@@ -12,3 +12,17 @@ END;
 --SELECT*FROM Employee;
 
 --Task – Create a stored procedure that returns the managers of an employee.
+CREATE OR REPLACE PROCEDURE updateProcedure2
+AS
+managerCursor SYS_REFCURSOR;
+BEGIN
+    OPEN managerCursor FOR
+    SELECT ReportsTo, FirstName
+    FROM Employee;
+    --WHERE ReportsTo = EmployeeId;
+    
+    DBMS_SQL.RETURN_RESULT(managerCursor);
+END;
+/
+
+EXEC updateProcedure2;
