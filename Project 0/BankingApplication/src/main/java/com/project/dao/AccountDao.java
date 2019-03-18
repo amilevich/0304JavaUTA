d86f5ef09e154e00f7ecf433b46695514f5c68d6
Kevin Sung <s.h.kevin.sung@gmail.com>
2019-03-18 16:21:43 -0500

    major edits

---
+++ Project 0/BankingApplication/src/main/java/com/project/dao/AccountDao.java
@@ -0,0 +1,17 @@
+package com.project.dao;
+
+import java.util.List;
+
+import com.project.account.Account;
+import com.project.users.User;
+
+public interface AccountDao {
+	public int insertAccount(Account P);
+	public Account selectAccount(String userName,String password, String name, String type);
+	public float selectAccount(String userName);
+	public int updateAccountAmount(Account p);
+	public int updateAccountAmount(String u,float p);
+	public int updateAccountStatus(String u, String s);
+	public int deleteAccount(Account p);
+	public int deleteAccount(String u); //for customer and admin
+}
