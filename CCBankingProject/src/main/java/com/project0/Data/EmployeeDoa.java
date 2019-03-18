3f3c6e2cfdda2f43784a8d0b83aa770ff8e4df9a
unknown <cmalexanderc@gmail.com>
2019-03-18 16:09:55 -0500

    Fixing project

--- CCBankingProject/src/main/java/com/project0/Data/EmployeeDoa.java
+++
@@ -1,9 +0,0 @@
-package com.project0.Data;
-
-import com.project0.Build.Employee;
-
-public interface EmployeeDoa {
-		
-		//Select from database by Username and Password
-		public Employee findEmployee(String Uname, String pass);
-}
