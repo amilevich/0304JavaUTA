762775811a3bfa845fade293e0575cb291b803af
amilevich <alexandra.i.milevich@gmail.com>
2019-03-28 12:52:24 -0500

    pets front controller

---
+++ PetsFrontController/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.model.Pet;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HomeController {
+
+	public static String Home(HttpServletRequest request) {
+		return null;
+	}
+	
+	public static String PetJSON(HttpServletRequest request, HttpServletResponse response) {
+		Pet pet = (Pet)request.getSession().getAttribute("Pet");
+		try {
+			response.getWriter().write(new ObjectMapper().writeValueAsString(pet));
+			/*
+			 * Marshalling (similar to serialization) is the process
+			 * of transforming memory representation of an object to
+			 * a data format suitable for storage or transmission.
+			 * Object -> JSON
+			 * Unmarshalling -> JSON to Object
+			 * 
+			 * Tools: Jackson (JSON marshalling tool)
+			 */
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
