5902fc037ab2003a5499f56c886bfab3aa917849
Angello Castro <angello.castro.002@gmail.com>
2019-04-02 22:19:42 -0700

    Revert "Registration and Login updated with password hashing"
    This reverts commit 59366fdbc71ab481f8cd518581dc94b5c01c24f3.

--- Project1ERS/src/main/java/com/ers/controller/RegisterController.java
+++ Project1ERS/src/main/java/com/ers/controller/RegisterController.java
@@ -1,9 +1,5 @@
 package com.ers.controller;
 
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-
 import javax.servlet.http.HttpServletRequest;
 
 import com.ers.dao.UserDaoImpl;
@@ -18,12 +14,7 @@ public class RegisterController {
 		System.out.println(name);
 		System.out.println(password);
 		
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		md.update(name.getBytes(StandardCharsets.UTF_8)); 		// 'salt' with username
-		byte[] digest = md.digest(password.getBytes(StandardCharsets.UTF_8));	// hash pw
-		String shaPassword = String.format("%064x", new BigInteger(1, digest));
-		
-		User user = new User(name, shaPassword, "Employee");
+		User user = new User(name, password, "Employee");
 		
 		UserDaoImpl userDaoImpl = new UserDaoImpl();
 		userDaoImpl.insertUser(user);
